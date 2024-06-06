afc42f700af4dac178c955e33644ca0a01f1a093
KyleHelms4415 <113382650+kyleb4415@users.noreply.github.com>
2024-06-06 12:03:20 -0400

    Add files via upload

---
+++ Russian-Roulette/ConsoleApp4/Gun.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Gun
+    {
+        public int NumChambers { get; set; }
+        public int NumBulletsToLoad { get; set; }
+        public int CurrentChamber { get; set; }
+
+        public int[] LoadedChambers { get; set; }
+        public int fire;
+
+        public Gun()
+        {
+            LoadedChambers = new int[6];
+            NumChambers = 6;
+            NumBulletsToLoad = 1;
+            CurrentChamber = 0;
+        }
+
+        public override string ToString()
+        {
+            return this.GetType().Name;
+        }
+
+        public void Load(int numBullets, int numChambers)
+        {
+            int loadedBullets = 0;
+            this.LoadedChambers = new int[numChambers];
+            this.NumChambers = numChambers;
+
+            if (numBullets > numChambers)
+            {
+                numBullets = numChambers;
+            }
+
+            this.NumBulletsToLoad = numBullets;
+
+            //this part will load the bullets in chambers
+            Random rand = new Random();
+            do
+            {
+                int chamberToLoad = rand.Next(0, numChambers);
+                if (LoadedChambers[chamberToLoad] != 1)
+                {
+                    LoadedChambers[chamberToLoad] = 1;
+                    loadedBullets++;
+                }
+            }
+            while (loadedBullets < NumBulletsToLoad);
+        }
+    }
+}
